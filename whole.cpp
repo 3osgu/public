94978d7f244089093579421be339ea0b5eab63bf
3osgu <chinas1@outlook.es>
2024-07-01 14:33:30 +0800

    Modify whole.cpp

--- whole.cpp
+++ whole.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Young something key action dinner spring reflect.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
