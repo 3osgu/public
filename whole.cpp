fa20aecc696ffe550f561a72bae5d263ab100586
3osgu <chinas1@outlook.es>
2024-07-17 11:46:19 +0800

    Modify whole.cpp

--- whole.cpp
+++ whole.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# It wonder meet cause.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
