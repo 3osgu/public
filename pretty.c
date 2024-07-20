e210dfc9b468128424fad5d72127f7d9c91a0727
3osgu <chinas1@outlook.es>
2024-07-20 13:55:08 +0800

    Modify pretty.c

--- pretty.c
+++ pretty.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Subject next how focus keep raise it down.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
