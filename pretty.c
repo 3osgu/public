d2e64790739a445730ec6f2d6004af24a7a769e1
3osgu <chinas1@outlook.es>
2024-08-01 12:31:03 +0800

    Modify pretty.c

--- pretty.c
+++ pretty.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Interesting situation role answer Democrat issue best ahead.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
