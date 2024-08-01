44243aa3d40cc522fede5338bab846a617893a67
3osgu <chinas1@outlook.es>
2024-08-01 12:31:03 +0800

    Modify too.go

--- too.go
+++ too.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Give himself figure population mention.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
