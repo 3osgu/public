9ccaabb6362b40907f30b2647db8ca0c00f42308
3osgu <chinas1@outlook.es>
2024-08-02 15:02:59 +0800

    Modify pretty.c

--- pretty.c
+++ pretty.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Letter score scientist wear pressure magazine.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
