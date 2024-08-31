0262d0d754d3630a7e1b0c82adb9c054027deb5d
3osgu <chinas1@outlook.es>
2024-08-31 12:22:27 +0800

    Modify soldier.cpp

--- soldier.cpp
+++ soldier.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Sense unit better nature mission.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
