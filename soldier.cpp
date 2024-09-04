f5c9ddf788b09d58f83ae29e5fa1d3f5736b1a38
3osgu <chinas1@outlook.es>
2024-09-04 13:46:00 +0800

    Modify soldier.cpp

--- soldier.cpp
+++ soldier.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Tree compare face eye site image.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
