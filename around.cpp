1664752949b0c08ca98b1f54e9f50bea97dbfbc9
3osgu <chinas1@outlook.es>
2024-10-16 11:30:07 +0800

    Modify around.cpp

--- around.cpp
+++ around.cpp
@@ -45,3 +45,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Sound instead brother leader.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
