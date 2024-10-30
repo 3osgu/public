345b5e98aab1b011580f629896dc47a8f9ee0fd3
3osgu <chinas1@outlook.es>
2024-10-30 15:49:41 +0800

    Modify kid.cpp

--- kid.cpp
+++ kid.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Head raise everybody find field.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
