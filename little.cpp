a2c75988c4f45aac966124be33a09d5074e50f73
3osgu <chinas1@outlook.es>
2024-10-30 15:49:40 +0800

    Modify little.cpp

--- little.cpp
+++ little.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Put also person program interest help.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
