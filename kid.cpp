395c3befb2c75c71fd079df48e2a1144e68b6e04
3osgu <chinas1@outlook.es>
2024-11-01 12:58:07 +0800

    Modify kid.cpp

--- kid.cpp
+++ kid.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Describe ahead especially produce local personal fear.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
