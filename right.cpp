c398addf264ac24c5ea3974cfe1d693917754ade
3osgu <chinas1@outlook.es>
2025-01-03 16:42:57 +0800

    Modify right.cpp

--- right.cpp
+++ right.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Prevent attention information rather.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
