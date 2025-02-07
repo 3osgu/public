d8b2c62282640a927d89b56472ad586fa1962de2
3osgu <chinas1@outlook.es>
2025-02-07 13:00:29 +0800

    Modify word.cpp

--- word.cpp
+++ word.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Produce again prove.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
