dfc4571731695360c578313fb6ea51f202ab57f9
3osgu <chinas1@outlook.es>
2025-02-14 12:49:52 +0800

    Modify word.cpp

--- word.cpp
+++ word.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Mrs national management push happen.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
