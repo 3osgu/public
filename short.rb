7fe4a2d66afeea230111e0d65d56f818ad139d2e
3osgu <162894228+3osgu@users.noreply.github.com>
2025-03-26 14:42:22 +0800

    test(core): add tests for short.rb

--- short.rb
+++ short.rb
@@ -1,1 +1,10 @@
-puts "Hello, World!"
+#!/usr/bin/env ruby
+# Short program demonstrating basic Ruby syntax
+
+def greet(name = "World")
+  puts "Hello, #{name}!"
+end
+
+if __FILE__ == $0
+  greet
+end
